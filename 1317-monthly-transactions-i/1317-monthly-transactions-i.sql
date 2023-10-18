3fa120be4f586423a516074909d8d5b5a34ca1d0
LeonQuadros <leonquaddy@gmail.com>
2023-10-18 02:04:42 -0400

    Time: 2192 ms (77.24%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 
 
-SELECT FORMAT(trans_date,'yyyy-MM') month,country, COUNT(id) trans_count, SUM(amount) trans_total_amount, SUM(CASE WHEN state='approved'THEN 1 ELSE 0 END) approved_count, SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) approved_total_amount
+SELECT LEFT(trans_date, '7') month,country, COUNT(id) trans_count, SUM(amount) trans_total_amount, SUM(CASE WHEN state='approved'THEN 1 ELSE 0 END) approved_count, SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) approved_total_amount
 FROM Transactions
-GROUP BY FORMAT(trans_date,'yyyy-MM'),country
+GROUP BY LEFT(trans_date, '7'),country
