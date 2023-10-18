a8246e1b8028c0d21de3dfc8c2181a5cdd684441
LeonQuadros <leonquaddy@gmail.com>
2023-10-18 01:54:47 -0400

    Time: 3273 ms (28.98%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
+
 WITH CTE AS(
 SELECT FORMAT(trans_date,'yyyy-MM') month,country, amount, CASE WHEN state='approved'THEN 1 ELSE 0 END approved_count, CASE WHEN state='approved' THEN amount ELSE 0 END approved_total_amount
 FROM Transactions
