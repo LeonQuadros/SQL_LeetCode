4ffad5f5c6ab251efb23e08501629aa9f77dcc2a
LeonQuadros <leonquaddy@gmail.com>
2023-10-24 18:48:56 -0400

    Time: 5630 ms (42.62%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,10 +1,9 @@
 /* Write your T-SQL query statement below */
 
-WITH CTE AS(
+SELECT C.product_id, C.first_year, S.quantity, S.price
+FROM 
+(
 SELECT product_id, MIN(year) AS first_year
 FROM Sales
-GROUP BY product_id)
-
-SELECT C.product_id, C.first_year, S.quantity, S.price
-FROM CTE C 
+GROUP BY product_id) C
 JOIN Sales S ON C.product_id = S.product_id and C.first_year = S.year
