50150ee58ff9ec8f7947c82453ab013a39bbc5e7
LeonQuadros <leonquaddy@gmail.com>
2023-10-26 19:53:38 -0400

    Time: 1286 ms (34.69%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -3,7 +3,6 @@
 WITH CTE AS(
 SELECT id, num num1, LEAD(num,1,0) OVER(ORDER BY id) num2, LEAD(num,2,0) OVER(ORDER BY id) num3
 FROM Logs)
-
 SELECT DISTINCT num1 ConsecutiveNums
 FROM CTE
 WHERE num1 = num2 AND num2 = num3 AND id NOT IN (SELECT MAX(id) FROM Logs)
