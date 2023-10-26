9df282563db0c84c04ddca8b19672490bddbbdc7
LeonQuadros <leonquaddy@gmail.com>
2023-10-25 20:46:54 -0400

    Time: 2096 ms (88.97%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,10 +1,9 @@
 /* Write your T-SQL query statement below */
 
-WITH CTE AS(
-SELECT turn Turn, person_id ID, person_name,weight Weight, SUM(Weight) OVER (ORDER BY turn ASC) Total_Weight
-FROM Queue)
 
 SELECT TOP 1 person_name
-FROM CTE
+FROM (
+SELECT turn Turn, person_id ID, person_name,weight Weight, SUM(Weight) OVER (ORDER BY turn ASC) Total_Weight
+FROM Queue) CTE
 WHERE Total_Weight <= 1000
 ORDER BY Total_Weight DESC
