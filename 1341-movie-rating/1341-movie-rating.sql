8e6f687d882ad758884497bfe68f688080be6a36
LeonQuadros <leonquaddy@gmail.com>
2023-10-27 19:15:59 -0400

    Time: 952 ms (64.69%), Space: 0B (100.00%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,19 @@
+/* Write your T-SQL query statement below */
+WITH CTE1 AS(
+SELECT TOP 1 U.name results
+FROM Users U
+JOIN MovieRating MR ON U.user_id = MR.user_id
+GROUP BY U.name
+ORDER BY COUNT(rating) DESC,U.name)
+,
+CTE2 AS(
+SELECT TOP 1 M.title results
+FROM MovieRating MR
+JOIN Movies M ON M.movie_id = MR.movie_id
+WHERE MR.created_at BETWEEN '02-01-2020' AND '02-29-2020'
+GROUP BY M.title
+ORDER BY ROUND(AVG(CAST(rating AS FLOAT)),4) DESC, M.title)
+
+SELECT * FROM CTE1
+UNION ALL
+SELECT * FROM CTE2
