1ead32d48eb64f5d813d0d83bbbccbf9aabea2e8
LeonQuadros <leonquaddy@gmail.com>
2023-10-29 03:00:00 -0400

    Time: 860 ms (30.23%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,13 @@
+/* Write your T-SQL query statement below */
+WITH CTE1 AS(
+SELECT visited_on, CAST(SUM(amount) AS FLOAT) amount, ROW_NUMBER() OVER(ORDER BY visited_on) rn
+FROM Customer
+GROUP BY visited_on)
+,
+CTE2 as(
+SELECT visited_on, rn, SUM(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) amount, ROUND(CAST(AVG(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS FLOAT),2) average_amount
+FROM CTE1)
+
+SELECT visited_on, amount, average_amount FROM CTE2
+WHERE rn > 6
+
